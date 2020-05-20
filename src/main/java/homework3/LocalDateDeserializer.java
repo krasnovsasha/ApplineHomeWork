4233b51014a67fcd9b473ee10ffb6749c8cced2d
alexander <pancasancha4@gmail.com>
2020-05-20 19:23:40 +0300

    -optimized parseStringToDate(return object)

--- src/main/java/homework3/LocalDateDeserializer.java
+++ src/main/java/homework3/LocalDateDeserializer.java
@@ -4,17 +4,18 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
+
 import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Scanner;
 
 class LocalDateDeserializer implements JsonDeserializer<LocalDate> {
 	@Override
 	public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
 		return LocalDate.parse(json.getAsString(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 	}
+
 	static LocalDate parseStringToDate(String text) {
 		DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd.MM.yy");
 		DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
